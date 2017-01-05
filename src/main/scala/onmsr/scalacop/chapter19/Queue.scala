43a6ce4c5def418341e449b14d2c564baa7f30b7
onmsr <masaru.onuma1004@gmail.com>
2017-01-05 10:57:11 +0900

    19章

---
+++ src/main/scala/onmsr/scalacop/chapter19/Queue.scala
@@ -0,0 +1,34 @@
+package onmsr.scalacop.chapter19
+
+class SlowAppendQueue[T](elems: List[T]) {
+  def head = elems.head
+  def tail = new SlowAppendQueue(elems.tail)
+  def enqueue(x: T) = new SlowAppendQueue(elems ::: List(x)) // 処理時間がO(N)
+}
+
+// リスト逆転版
+class SlowHeadQueue[T](elems: List[T]) {
+  def head = elems.last // 処理時間がO(N)
+  def tail = new SlowAppendQueue(elems.init) // 処理時間がO(N)
+  def enqueue(x: T) = new SlowAppendQueue(x :: elems)
+}
+
+// ハイブリッド版
+// 1 2 3 4 5 6 -> (1 2 3) (6 5 4)
+// 後半部分を逆転してもっておく
+class Queue[T](
+  private val leading: List[T],
+  private val trailing: List[T]
+) {
+  /**
+   * キューを返す。先頭部分が空のときに後半部分を前半部分に移し替える。
+   * 先頭部分が空のときに処理時間がO(N)だが、待ち行列が長くなれば呼び出し回数は少ない。
+   */
+  private def mirror = if (leading.isEmpty) new Queue(trailing.reverse, Nil) else this
+  def head = mirror.leading.head
+  def tail = {
+    val q = mirror
+    new Queue(q.leading.tail, q.trailing)
+  }
+  def enqueue(x: T) = new Queue(leading, x :: trailing)
+}
---
+++ src/test/scala/onmsr/scalacop/chapter19/QueueSpec.scala
@@ -0,0 +1,12 @@
+package onmsr.scalacop.chapter19
+
+import org.specs2.mutable.Specification
+
+class QueueSpec extends Specification {
+  "QueueSpec" should {
+    "test" in {
+      
+      true must_== false
+    }
+  }
+}
