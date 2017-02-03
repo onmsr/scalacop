6a5e00923abe07ffd8b5537a8d19d31befe34449
onuma <masaru_onuma@dwango.co.jp>
2017-02-03 12:04:04 +0900

    29章追加

---
+++ src/main/scala/onmsr/scalacop/chapter29/Main.scala
@@ -0,0 +1,16 @@
+package onmsr.scalacop.chapter29
+
+object Main {
+  def main(args: Array[String]) = {
+    
+  }
+}
+
+/**29章まとめ
+ * モジュール化で大事なこと
+ * 1. インターフェースと実装を分離
+ * 2. インターフェースを交換可能
+ *
+ * シングルトン型(よくわからない)
+ *
+ */
